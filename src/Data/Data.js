b315679b7107b50b907e7ef4113a448693302cb1
Henrie-Nguyen <henrie2911@gmail.com>
2024-11-28 10:27:46 -0800

    online education

---
+++ src/Data/Data.js
@@ -0,0 +1,182 @@
+
+// React Icons For Nav And React.......................
+import { FiUser } from 'react-icons/fi';
+import { VscSearch } from 'react-icons/vsc';
+import { BsBag } from 'react-icons/bs';
+
+// Nav [a] Links  Data.......................
+export const navLinks = [
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name: 'About',
+    path: '/about'
+  },
+  {
+    name: 'Courses',
+    path: '/courses'
+  },
+  {
+    name: 'Our Service',
+    path: '/ourservice'
+  },
+  {
+    name: 'Contact us',
+    path: '/contactus'
+  },
+  {
+    name: 'Sign in',
+    path: '/signin'
+  },
+];
+
+
+
+
+
+
+// navRight links Data..............................
+export const navRight = {
+  managements: [
+    {
+      id: 1,
+      icon: FiUser,
+      link: '*'
+    },
+    {
+      id: 2,
+      icon: BsBag,
+      link: '*'
+    },
+    {
+      id: 3,
+      icon: VscSearch,
+      link: '*'
+    },
+  ]
+
+};
+
+
+
+
+// Header Card  Data.......................
+import bookgirl1 from '../assets/Group337.png';
+import bookgirl2 from '../assets/Group339(1).png';
+import bookgirl3 from '../assets/Group339.png';
+
+export const bookgirls = [
+  {
+    id: 1,
+    img: bookgirl1,
+    title: 'UI/UX design for Beginners',
+    price: '98$',
+    hours: '22 Hr 30 min',
+    Cource: '34 Cource',
+    Sales: '314 Sales'
+  },
+  {
+    id: 2,
+    img: bookgirl2,
+    title: 'UI/UX design for Beginners',
+    price: '98$',
+    hours: '22 Hr 30 min',
+    Cource: ' 34 Cource',
+    Sales: '314 Sales'
+  },
+  {
+    id: 3,
+    img: bookgirl3,
+    title: 'UI/UX design for Beginners',
+    price: '98$',
+    hours: '22 Hr 30 min',
+    Cource: ' 34 Cource',
+    Sales: '314 Sales'
+  },
+];
+
+// Card Data----------
+
+
+import traickgirl from '../assets/Group337(1).png';
+import traickgirl2 from '../assets/Group338(1).png';
+import traickgirl3 from '../assets/Group339(1).png';
+
+
+export const taickgirls = [
+  {
+    id: 1,
+    img: traickgirl,
+    title: 'Matthew E. McNatt',
+    professor: 'Professor @George Brown College',
+    profession: 'Engineering physics',
+  
+  },
+  {
+    id: 2,
+    img: traickgirl2,
+    title: 'Tracy D. Wright',
+    professor: 'Professor @George Brown College',
+    profession: 'Engineering physics',
+
+  },
+  {
+    id: 3,
+    img: traickgirl3,
+    title: 'Cynthia A. Nelson',
+    professor: 'Professor @George Brown College',
+    profession: 'Engineering physics',
+
+  },
+];
+
+// Testimals Data.......................
+
+import avtergirl from '../assets/woman.png';
+import man from '../assets/man.png';
+import hacker from '../assets/hacker.png';
+import user from '../assets/user.png';
+import woman from '../assets/woman(1).png';
+
+export const tesimalsData = [
+  {
+    id: 1,
+    img: woman,
+    description: '“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio dapibus facilisis sociis natoque penatibus.”',
+    name: 'Coriss Ambady',
+    profesion: 'Financial Analyst',
+  },
+  {
+    id: 2,
+    img: avtergirl,
+    description: '“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio dapibus facilisis sociis natoque penatibus.”',
+    name: 'Coriss Ambady',
+    profesion: 'Financial Analyst',
+  },
+  {
+    id: 3,
+    img: man,
+    description: '“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio dapibus facilisis sociis natoque penatibus.”',
+    name: 'Coriss Ambady',
+    profesion: 'Financial Analyst',
+  },
+  {
+    id: 4,
+    img: hacker,
+    description: '“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio dapibus facilisis sociis natoque penatibus.”',
+    name: 'Coriss Ambady',
+    profesion: 'Financial Analyst',
+  },
+  {
+    id: 5,
+    img: user,
+    description: '“Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Cras justo odio dapibus facilisis sociis natoque penatibus.”',
+    name: 'Coriss Ambady',
+    profesion: 'Financial Analyst',
+  },
+  
+];
+
+
